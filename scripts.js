cadcdfd0677d35854e380aa8e92bbf60e06b792c
Johao Lopez <johao.lopz.m@gmail.com>
2016-03-20 20:59:46 -0500

    Errores en archivo js, corregidos

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 console.log("Inicio de programa");
-alert("Hola JavaScript);
-document.write("<h1>Hola JS UPSE</p>");
-alet("Gracias por visitar");
-console.log("Fin de programa")
+alert("Hola JavaScript");
+document.write("<h1>Hola JS UPSE</h1>");
+alert("Gracias por visitar");
+console.log("Fin de programa");
